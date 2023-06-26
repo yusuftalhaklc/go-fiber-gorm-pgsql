e49d44e8f6bab4294254cd4befccd06fb2c7958e
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-26 10:56:14 +0300

    Add database package with database.go file

--- database/database.go
+++ database/database.go
@@ -1,1 +1,47 @@
 package database
+
+import (
+	"fmt"
+	"go-fiber/config"
+	"go-fiber/model"
+	"log"
+	"os"
+	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBinstance struct {
+	DB *gorm.DB
+}
+
+var DB DBinstance
+
+func Connect() {
+	p := config.Config("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+	if err != nil {
+		fmt.Println("Error parsing str to int")
+	}
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/Istanbul", config.Config("DB_HOST"), config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"), port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected")
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("running migrations")
+
+	db.AutoMigrate(&model.User{})
+	DB = DBinstance{
+		DB: db,
+	}
+}
