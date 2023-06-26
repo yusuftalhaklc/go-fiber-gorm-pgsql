6182c57e6b26a0f2eac564c64bbbd828d06cb314
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-26 11:08:07 +0300

    Add router setup for routes

--- router/route.go
+++ router/route.go
@@ -1,1 +1,19 @@
 package router
+
+import (
+	"go-fiber/handler"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+
+	api := app.Group("/api")
+	v1 := api.Group("/user")
+
+	v1.Get("/", handler.GetAllUsers)
+	v1.Get("/:id", handler.GetSingleUser)
+	v1.Post("/", handler.CreateUser)
+	v1.Put("/:id", handler.UpdateUser)
+	v1.Delete("/:id", handler.DeleteUserByID)
+}
