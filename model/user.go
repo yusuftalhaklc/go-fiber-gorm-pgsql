51609930c230b14c117b2ffb6d2dd0ca7f484a23
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-26 10:56:58 +0300

    Add user model implementation

--- model/user.go
+++ model/user.go
@@ -1,1 +1,23 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID       uuid.UUID `gorm:"type:uuid;"`
+	Username string    `json:"username"`
+	Email    string    `json:"email"`
+	Password string    `json:"password"`
+}
+
+type Users struct {
+	Users []User `json:"users"`
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.ID = uuid.New()
+	return
+}
